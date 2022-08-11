19eecff50c84e71dc1c6da2a712afcfed4dfd896
Adrian Arturo Mora <85296570+adrianarturomora@users.noreply.github.com>
2022-08-10 21:34:07 -0400

    I have figured out how to ask for the users information and append it to a text file.

--- src/main.java
+++ src/main.java
@@ -6,17 +6,12 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args)
     {
-
-
         bank_Options();
-        user_Option_Selection(int user_Input, Scanner scanner);
+        user_Option_Selection();
     }
 
     public static void bank_Options()
     {
-        Scanner scanner = new Scanner(System.in);
-        int user_Input;
-
         System.out.println("Welcome to Adrian's Bank! Please select of the options below: ");
         System.out.println("1. Account Opening");
         System.out.println("2. Withdrawal");
@@ -24,33 +19,45 @@ public class main {
         System.out.println("4. Check Balance");
         System.out.println("5. Delete Account");
         System.out.println("6. Update Account");
+    }
+
+    public static void user_Option_Selection()
+    {
+        Scanner scanner = new Scanner(System.in);
+        int user_Input;
 
         System.out.println("Welcome to Adrian's Bank! Please select of the options listed above: ");
         user_Input = scanner.nextInt();
-        user_Option_Selection(user_Input, scanner);
-    }
 
-    public static void user_Option_Selection(int user_Input, Scanner scanner)
-    {
         if(user_Input == 1)
         {
-           // String first_Name;
-            String last_Name;
-            String username;
-            String Password;
-            String email_Address;
+            Scanner scanner2 = new Scanner(System.in);
 
             System.out.println("First Name: ");
-            String first_Name = scanner.nextLine();
+            String first_Name = scanner2.nextLine();
+
+            System.out.println("Last Name: ");
+            String last_Name = scanner2.nextLine();
+
+            System.out.println("Username: ");
+            String username = scanner2.nextLine();
+
+            System.out.println("Password: ");
+            String password = scanner2.nextLine();
+
+            System.out.println("Last Name: ");
+            String email_Address = scanner2.nextLine();
+
 
             try {
-                BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
-                writer.write(first_Name);
+                BufferedWriter writer = new BufferedWriter(new FileWriter("output3.txt"));
+                writer.append(first_Name + " " + last_Name + " " + username + " " + password + " " + email_Address);
                 writer.newLine();
                 writer.close();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+        }
 /*
             System.out.println("Last Name: ");
 
@@ -64,10 +71,8 @@ public class main {
 
             System.out.println("Initial Deposit: ");
 */
-        }else
-            System.exit(0);
+        }
     }
 
 
-}
 
